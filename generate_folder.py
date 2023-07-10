7688d01b2960237801d43810f644b5956bb6204f
Mango <evan940415@gmail.com>
2023-07-10 17:45:15 +0800

    fix: indent

--- generate_folder.py
+++ generate_folder.py
@@ -1,25 +1,24 @@
 import os
 
 while 1:
-    name = input('Project Name')
-    folder_path = 'src/' + name.replace(" ", "_")
+    name = input("Project Name: ")
+    folder_path = "src/" + name.replace(" ", "_")
 
     try:
         os.mkdir(folder_path)
     except FileExistsError:
         pass
 
-    with open(f'{folder_path}/README.md', 'w', encoding='utf8') as file:
-        file.write(f"""# {name} | ...
+    with open(f"{folder_path}/README.md", "w", encoding="utf8") as file:
+        file.write(
+            f"# {name} | ...\n\n"
+            "...\n\n"
+            "## 前置\n"
+            "- 模組\n"
+            "  - ...\n"
+            "- KubeJS本地通用庫\n"
+            "  - ..."
+        )
 
-    ...
-
-    ## 前置
-
-    - 模組
-    - ...
-    - KubeJS本地通用庫
-    - ...""")
-    
-    if input('continue?').lower() in {'n', 'no'}:
+    if input("continue?\n  ").lower() in {"n", "no"}:
         break
