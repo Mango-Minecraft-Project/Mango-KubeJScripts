18219e26262cf859a63b99d9519b77abd6f432e5
Mango <evan940415@gmail.com>
2024-06-08 08:13:47 +0800

    add new feature

--- generate_list.py
+++ generate_list.py
@@ -1,6 +1,9 @@
+import json
 from pathlib import Path
 
 SCRIPT_DIR = Path("scripts")
 
 for script_dir in SCRIPT_DIR.iterdir():
-    print(f"- [{script_dir.name.replace("_", " ")}](./{script_dir.name})")
+    with open(script_dir / ".kjspkg", encoding="utf8") as f:
+        author = json.load(f)["author"]
+    print(f"- [{script_dir.name.replace("_", " ")} by {author}](./{script_dir.name})")
